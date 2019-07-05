38609934144a4a9f96ff5cacaab97226cf4a6fb4
zhiguangli <252859182@qq.com>
2019-07-05 11:32:54 +0800

    Update IntegerList.hpp
    添加注释和测试用例

--- IntegerList.hpp
+++ IntegerList.hpp
@@ -132,16 +132,32 @@ struct RemoveValue<List<First, Rest...>, Value>
 
 int main()
 {
-    std::cout << Max<List<1,2,44,4,6>>::value << std::endl;
-    std::cout << Max<Append<List<1,2,44,4,6>, 55>::type>::value << std::endl;
-    std::cout << "type_list is same: " << std::is_same_v<Append<List<1,2,44,4,6>, 55>::type, List<1,2,44,4,6, 55>> << std::endl;
-    std::cout << "type_list index 2 element: "  << FindIndexElement<List<11, 22, 33, 44, 55, 66, 77, 88>, 2>::value << std::endl;
-    std::cout << "type_list concat is same: " << std::is_same_v<Concat<List<1, 2, 3>, List<4, 5, 6>>::type, List<1, 2, 3, 4, 5, 6>> << std::endl;
-    
-    std::cout << "type_list modify is same: " << std::is_same_v<Modify<List<1, 2, 3, 4, 5, 6>, 2, 10>::type, List<1, 2, 10, 4, 5, 6>> << std::endl;
-    std::cout << "type_list remove 4 index is same: " << std::is_same_v<Remove<List<1, 2, 3, 4, 5, 6>, 4>::type, List<1, 2, 3, 4, 6>> << std::endl; 
-    
-    std::cout << "type_list remove value 3 is same: " << std::is_same_v<RemoveValue<List<1, 2, 3, 4, 3, 3>, 3>::type, List<1, 2, 4>> << std::endl; 
+    using TestList1 = List<1, 2, 3, 4, 5, 6>;
+    //Max最大值
+    std::cout << "TestList1 max value is: " <<  Max<TestList1>::value << std::endl;  //output 6.
+    //尾部添加一个元素后，并输出最大值
+    using TestList2 = Append<TestList1, 7>::type;
+    std::cout << "max value is: " << Max<TestList2>::value << std::endl;  //output 7.
+    //判断添加后的类型是否一致
+    std::cout << "TestList2 is same: " << std::is_same_v<TestList2, List<1, 2, 3, 4, 5, 6, 7>> << std::endl;
+    //查找TestList2的第2个元素
+    std::cout << "TestList2 index 2 element is: "  << FindIndexElement<TestList2, 2>::value << std::endl; //output 3
+    //连接两个列表成一个列表
+    using List1 = List<1, 2, 3>;
+    using List2 = List<4, 5, 6>;
+    std::cout << "concat is same: " << std::is_same_v<Concat<List1, List2>::type, List<1, 2, 3, 4, 5, 6>> << std::endl;
+    //根据下标改变列表的值
+    std::cout << "TestList2 modify index 2 value to 10 is same: " << std::is_same_v<Modify<List<1, 2, 3, 4, 5, 6>, 2, 10>::type, List<1, 2, 10, 4, 5, 6>> << std::endl;
+    //根据下标删除元素
+    using Remove_Index_4_List = Remove<List<1, 2, 3, 4, 5, 6>, 4>::type;  //删除了数字5.
+    std::cout << "type_list remove index[4] is same: " << std::is_same_v<Remove_Index_4_List, List<1, 2, 3, 4, 6>> << std::endl; 
+    //根据值删除列表下搜索符合的值
+    using Remove_Value_3_List= RemoveValue<List<1, 2, 3, 4, 3, 3>, 3>::type;
+    std::cout << "type_list remove value 3 is same: " << std::is_same_v<Remove_Value_3_List, List<1, 2, 4>> << std::endl; 
     return 0;
 }
+
+
+
+
 
