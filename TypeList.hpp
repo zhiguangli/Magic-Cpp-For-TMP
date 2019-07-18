7fbd9139066acacedecc9c0568f9f38d5aa8e247
zhiguangli <252859182@qq.com>
2019-07-18 20:27:50 +0800

    Update TypeList.hpp

--- TypeList.hpp
+++ TypeList.hpp
@@ -1,7 +1,10 @@
-#ifndef TYPELIST_HPP
-#define TYPELIST_HPP
-
+#include <iostream>
+#include <cstdlib>
+#include "typelist.hpp"
+#include <tuple>
+#include <cmath>
 #include <type_traits>
+
 
 //base opereator
 template<class> struct list_helper;
@@ -43,4 +46,28 @@ constexpr bool contain_v = list_helper<L>::template contain<T>;
 template<typename T, typename U>
 using concat_t = typename list_helper<T>::template concat<U>::type;
 
-#endif
+template<typename T>
+struct convert
+{
+    using type = double;
+};
+
+template<typename T>
+using convert_t = typename convert<T>::type;
+
+int main()
+{
+    using _source_tuple = std::tuple<int,int,int>;
+    using _return_tuple = map_t<_source_tuple, convert_t>;
+    using _expect_tuple = std::tuple<double, double, double>;
+    
+    std::cout << "is_same: " << std::is_same_v<_return_tuple, _expect_tuple> << std::endl;
+    std::cout << "length: " << length_v<_expect_tuple> << std::endl;
+    std::cout << "contain: " << contain_v<_expect_tuple, double> << std::endl;
+    
+    using _expect_concat_tuple = std::tuple<int, int, int, double, double, double>;
+    using _concat_after_tuple = concat_t<_source_tuple, _expect_tuple>;
+    std::cout << "is same: " << std::is_same_v<_concat_after_tuple, _expect_concat_tuple> << std::endl;
+}
+
+
